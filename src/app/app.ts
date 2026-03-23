e1bf2a834b7f4d53a089c07a11a6f56e4f60cf12
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2026-03-23 20:26:24 -0300

    responsividade adicionada

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './core/components/header/header';
 import { Hero } from './pages/home/components/hero/hero';
+import { About } from "./pages/home/components/about/about";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Hero],
+  imports: [Header, Hero, About],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
