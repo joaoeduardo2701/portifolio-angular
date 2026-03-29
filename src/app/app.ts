eb9341bd0e1b4f7ce35189fda9d7e9486b599f6a
João Eduardo Cardoso Padilha <duducardoso2701@gmail.com>
2026-03-29 20:32:50 -0300

    ajustes em alguns arquvios

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Header } from './core/components/header/header';
 import { Hero } from './pages/home/components/hero/hero';
 import { About } from "./pages/home/components/about/about";
+import { Skills } from "./skills/skills";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Hero, About],
+  imports: [Header, Hero, About, Skills],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
