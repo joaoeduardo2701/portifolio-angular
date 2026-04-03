1509f614895dd6615574f8726a89064c03a1bafe
Eduardo Cardoso <63940232+joaoeduardo2701@users.noreply.github.com>
2026-04-03 03:11:35 +0000

    links funcionando

--- src/app/core/components/header/header.ts
+++ src/app/core/components/header/header.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './header.scss',
 })
 export class Header {
-
+  scrollTo(elementId: string): void {
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      element.style.borderBottom = '1px';
+      element.style.color = 'white';
+    }
+  }
 }
