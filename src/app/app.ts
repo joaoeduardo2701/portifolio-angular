7a1abc81670316a34fb16cca5f617f674925c640
Eduardo Cardoso <63940232+joaoeduardo2701@users.noreply.github.com>
2026-04-03 02:16:21 +0000

    adição do footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { Header } from './core/components/header/header';
 import { Hero } from './pages/home/components/hero/hero';
 import { About } from './pages/home/components/about/about';
 import { Skills } from './pages/home/components/skills/skills';
+import { Footer } from './pages/home/components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Hero, About, Skills],
+  imports: [Header, Hero, About, Skills, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/pages/home/components/footer/footer.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Button } from '../../../../shared/components/button/button';
+
+@Component({
+  selector: 'app-footer',
+  imports: [Button],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss',
+})
+export class Footer {}
